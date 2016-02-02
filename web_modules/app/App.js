972e93ba8bb133aaf29ab1d3e451ab2b98e26df8
Maud MULLER <maudmuller@MacBook-Pro-de-Maud.local>
2016-02-02 19:14:04 +0100

    first commit

--- web_modules/app/App.js
+++ web_modules/app/App.js
@@ -1,11 +1,44 @@
 import React, { Component } from 'react';
 
-export default class App extends Component {
-  render() {
-    return (
-      <div>
-        Hello React !
-      </div>
-    );
-  }
+import Item from "item";
+import List from "list";
+
+const artists = [
+	{name: "bob marley"},
+	{name: "bob Dylan"},
+	{name: "odezenne"},
+	{name: "Kavinsky"}
+]
+
+const kinds = {
+	rap: {name: "rap"},
+	rock: {name: "rock"},
+	electro: {name: "electro"}
 }
+
+	/*
+				artists &&
+				artists.map((artist, index) => {
+					return this.getItem(artist,index)
+				})
+			}*/
+
+export default class App extends Component {
+	render() {
+		return (
+			<div>
+			<List items={artists} />
+			{
+				kinds &&
+				Object.keys(kinds).map ((key, index) => {
+					return(
+						<ArtistItem key={index} name={kinds key].name} />
+					)
+				})
+			}
+			</div>
+		);
+	}
+}
+	
+
---
+++ web_modules/item/index.js
@@ -0,0 +1,8 @@
+import React, {Component} from "react"
+
+const Item = ({name = ""}) => {
+	return <div className= "item">{name}</div>
+}
+
+export default Item
+
