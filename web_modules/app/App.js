23512c5dd069535c73f7d28ef1f959fd7f0a2b03
Maud MULLER <maudmuller@MacBook-Pro-de-Maud.local>
2016-02-09 15:00:32 +0100

    commit_fetch

--- web_modules/app/App.js
+++ web_modules/app/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import List from "List";
 
+/*
 const artists = [
   {name:"Bob Marley"},
   {name:"Bob Dylan"},
   {name:"Odezenne"},
   {name:"Kavinsky"}
 ]
+*/
 
 const kinds = {
   rap: {name:"Rap"},
@@ -17,10 +19,30 @@ const kinds = {
 const kindsArray = Object.keys(kinds).map((k) => { return kinds[k] });
 
 export default class App extends Component {
+
+  state = {
+    artists:[]
+  };
+
+  componentDidMount(){
+    console.log("")
+
+    fetch("https://api.spotify.com/v1/search?query=Bob*&offset=0&limit=10&type=artist").then((responsive)=> {
+      responsive.json().then((data) =>{
+        console.log(data)
+        if(!data.error){
+        console.log(data)
+        this.setState({artists:data.artists.items})
+        }
+      })
+    });
+
+  }
+
   render() {
     return (
       <div>
-        <List title="Artist" items={artists} />
+        <List title="Artist" items={this.state.artists} />
         <List title="Kind" items={kindsArray} />
       </div>
     )
